c76e0bc6a2af45132b9e9e566183518b504c6486
Jordan Caley <jordancaley10@gmail.com>
2021-04-22 12:05:12 +0200

    Event listener for umbrella color, add basic beach layout

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("I'm linked");
+
+const startButton = document.getElementById('start-btn');
+const umbrella = document.querySelector('.fas.fa-umbrella-beach');
+
+const handleClick = () => {
+  umbrella.classList.toggle('orchid');
+  console.log(umbrella.classList)
+}
+
+startButton.addEventListener("click", handleClick);
